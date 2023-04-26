9210be3af98b92d5ff6a817b6001b2ef5eba8cc2
LazimR <51827035+LazimR@users.noreply.github.com>
2023-04-26 14:28:42 -0300

    Create lig_desl.py
    Bagulho dos botão

---
+++ lig_desl.py
@@ -0,0 +1,27 @@
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+
+class MeuApp(App):
+    def build(self):
+        layout = GridLayout(cols=1, rows=2)
+
+        botao_ligar = Button(text="Ligar")
+        botao_ligar.bind(on_press=self.on_button_ligar_click)
+        layout.add_widget(botao_ligar)
+
+        botao_desligar = Button(text="Desligar")
+        botao_desligar.bind(on_press=self.on_button_desligar_click)
+        layout.add_widget(botao_desligar)
+
+        return layout
+
+    def on_button_ligar_click(self, instance):
+        print("Imagine que o programa ligou")
+
+    def on_button_desligar_click(self, instance):
+        print("Imagine que o programa desligou")
+
+if __name__ == '__main__':
+    MeuApp().run()
