380b991677c98942fdc01f1606845f7e9f9f7b57
mauriciobenjamin700 <mauriciobenjamin700@gmail.com>
2023-04-25 23:06:56 -0300

    preparação
    Imagens e pacotes necessários

---
+++ backend.py
@@ -0,0 +1,7 @@
+from pyautogui import press,write
+
+press('win')
+
+write(message='Chrome', interval=0.5)
+
+press('enter')
---
+++ testes.py
@@ -0,0 +1,13 @@
+import pyautogui
+#pyautogui.alert('This is an alert box.')
+
+pyautogui.confirm('Shall I proceed?')
+#'Cancel'
+"""
+pyautogui.confirm('Enter option.', buttons=['A', 'B', 'C'])
+'B'
+pyautogui.prompt('What is your name?')
+'Al'
+pyautogui.password('Enter password (text will be hidden)')
+'swordfish'
+"""
